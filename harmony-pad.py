44928a47ed4ffed29c8cde83cd47092adaca74c0
liubanlaobanzhang <3143786339@qq.com>
2022-12-30 21:04:57 +0800

    Ver1.0 [debug3,@2022.12.30]

--- harmony-pad.py
+++ harmony-pad.py
@@ -2,6 +2,10 @@
 import os
 import random
 import time
+qrcode=input('重新导入？')
+if qrcode==1:
+    os.system('adb shell rm /sdcard/Pictures/qrcode.jpg')
+    os.system('adb push qrcode.jpg /sdcard/Pictures')
 print('——————————————————————————————————————————')
 print('刷码 Ver1.0[debug3,@2022.12.28@Harmony OS(Pads)]')
 
@@ -31,30 +35,6 @@ r=0
 start=time.perf_counter() # 开始计时
 print('——————————————————————————————————————————')
 
-# ————————————【执行模块】开始 ————————————
-def 执行():
-        r=r+1
-        atime=time.perf_counter() #开始计时-循环
-        print('循环',r,'，模拟操作1')
-        os.system('adb shell input tap 1350 900') # 点击“扫一扫”
-        time.sleep(0.8)
-        print('循环',r,'，模拟操作2：选择照片')
-        os.system('adb shell input tap 1450 210') # 呼出照片库（在右上角）
-        time.sleep(0.75)
-        os.system('adb shell input tap 150 350') # 选择照片
-        time.sleep(0.25)
-        os.system('adb shell input tap 1500 150') # 确认照片
-        print('循环',r,'，等待链接跳转')
-        time.sleep(4.25)
-        print('循环',r,'，模拟操作3：返回')
-        os.system('adb shell input swipe 0 1450 350 1450 100')
-        os.system('adb shell input swipe 0 1450 350 1450 100')
-        btime=time.perf_counter() # 开始计时-循环
-        cycltime=round(btime-atime,3)
-        print('操作循环',r,'：完成，用时',cycltime,'秒')
-        print('——————————————————————————————————————————')
-# ————————————【执行模块】结束 ————————————
-
 for i in range(1,a):
     print('循环',i,'准备……')
     time.sleep(1)
@@ -67,8 +47,27 @@ for i in range(1,a):
                     input('检查设备状态！')
                 if i==randomcheck3:
                     input('检查设备状态！')
-        else:
-            执行()
+    r=r+1
+    atime=time.perf_counter() #开始计时-循环
+    print('循环',r,'，模拟操作1')
+    os.system('adb shell input tap 1350 900') # 点击“扫一扫”
+    time.sleep(0.8)
+    print('循环',r,'，模拟操作2：选择照片')
+    os.system('adb shell input tap 1450 210') # 呼出照片库（在右上角）
+    time.sleep(0.75)
+    os.system('adb shell input tap 150 350') # 选择照片
+    time.sleep(0.5)
+    os.system('adb shell input tap 1500 150') # 确认照片
+    print('循环',r,'，等待链接跳转')
+    time.sleep(3.5)
+    print('循环',r,'，模拟操作3：返回')
+    os.system('adb shell input swipe 0 1450 350 1450 100')
+    os.system('adb shell input swipe 0 1450 350 1450 100')
+    btime=time.perf_counter() # 开始计时-循环
+    cycltime=round(btime-atime,3)
+    print('操作循环',r,'：完成，用时',cycltime,'秒')
+    print('——————————————————————————————————————————')
+
 
 stop=time.perf_counter() # 结束计时
 time=round(stop-start,3) # 计算时间
