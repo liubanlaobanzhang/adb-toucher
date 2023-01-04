0f5160390c14ea04c56cfd56bc9257fb4372470b
liubanlaobanzhang <3143786339@qq.com>
2023-01-04 13:57:22 +0800

    改进 UI

--- harmony-pad-2.0-2301.py
+++ harmony-pad-2.0-2301.py
@@ -23,14 +23,14 @@ with Progress(TextColumn("[progress.description]{task.description}"),
               TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
               TimeRemainingColumn(),
               TimeElapsedColumn()) as progress:
-    总进度 = progress.add_task(description="总进度", total=a)
     每个循环 = progress.add_task(description="此循环", total=9)
+    总进度 = progress.add_task(description="总进度", total=a)
     for ep in range(1):
         for batch in range(a):
+                progress.reset(每个循环)
                 progress.advance(每个循环, advance=1)
                 time.sleep(0.5)
                 r=1
-                progress.reset(每个循环)
                 for step in range(9):
                     if r==1:
                         os.system('adb shell input tap 1350 900') # 点击“扫一扫”
