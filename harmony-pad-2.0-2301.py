f1f1178b1d371d253c57a1482386e7bb1887f97d
liubanlaobanzhang <3143786339@qq.com>
2023-01-09 14:06:29 +0800

    重传代码

--- harmony-pad-2.0-2301.py
+++ harmony-pad-2.0-2301.py
@@ -2,28 +2,33 @@ from rich.progress import Progress, TextColumn, BarColumn, TimeElapsedColumn, Ti
 import time
 import os
 
-print('——————————————————————————————————————————')
+total=0
+print('—————————————————————————————————————————————————')
 print('刷码 2.0.2301(Debug 3)@2023.1.4@Harmony OS(Pads)]')
+for qwert in range(114514):
+    for q in range(114514):# 检查设备列表
+        print('—————————————————————————————————————————————————')
+        os.system('adb devices')
+        os.system('adb shell input swipe 950 1450 950 850 100')
+        time.sleep(0.5)
+        os.system('adb shell input swipe 950 850 950 1450 100')
+        print('—————————————————————————————————————————————————')
+        print('检查屏幕是否有所滑动，一切正常则输入重复次数。输入0重显设备列表。')
+        a=int(input('重复次数：'))
+        if a!=0:
+            break
 
-for q in range(1,114514):# 检查设备列表
-    print('——————————————————————————————————————————')
-    os.system('adb devices')
-    os.system('adb shell input swipe 950 1450 950 850 100')
-    os.system('adb shell input swipe 950 850 950 1450 100')
-    print('——————————————————————————————————————————')
-    a=int(input('检查是否出现设备，一切正常则输入重复次数。输入0重显设备列表。'))
-    if a!=0:
-        break
+    with Progress(TextColumn("[progress.description]{task.description}"),
+                  BarColumn(),
+                  TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+                  TimeRemainingColumn(),
+                  TimeElapsedColumn()) as progress:
 
-with Progress(TextColumn("[progress.description]{task.description}"),
-              BarColumn(),
-              TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
-              TimeRemainingColumn(),
-              TimeElapsedColumn()) as progress:
-    总进度 = progress.add_task(description="总进度", total=a)
-    for ep in range(1):
-        每个循环 = progress.add_task(description="此循环", total=9)
-        for batch in range(a):
+        每个循环 = progress.add_task(description="此循环", total=13)
+        总进度 = progress.add_task(description="总进度", total=13*a)
+
+        for ep in range(1):
+            for batch in range(a):
                 progress.reset(每个循环)
                 progress.advance(每个循环, advance=1)
                 time.sleep(0.5)
@@ -35,12 +40,13 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                         time.sleep(0.8)
                     if r==3:
                         os.system('adb shell input tap 1450 210') # 呼出照片库（在右上角）
+                        time.sleep(0.3)
                     if r==4:
-                        time.sleep(0.5)
+                        time.sleep(0.2)
                     if r==5:
                         os.system('adb shell input tap 150 350') # 选择照片
                     if r==6:
-                        time.sleep(0.5)
+                        time.sleep(0.2)
                     if r==7:
                         os.system('adb shell input tap 1500 150') # 确认照片
                     if r==8:
@@ -61,5 +67,7 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                     r=r+1
 
                     progress.advance(每个循环, advance=1)
-                progress.advance(总进度, advance=1)
-                
+                    progress.advance(总进度, advance=1)
+    print('======== 完成 ========')
+    total=total+a
+    print('已进行',total,'次扫码行为。')
--- harmony-phone-2.0-2302-debug1.py
+++ harmony-phone-2.0-2302-debug1.py
@@ -2,26 +2,29 @@ from rich.progress import Progress, TextColumn, BarColumn, TimeElapsedColumn, Ti
 import time
 import os
 
-print('——————————————————————————————————————————')
+print('—————————————————————————————————————————————————')
 print('刷码 2.0.2301(Debug 1)@2023.1.4@Harmony OS(Phone)]')
+print('此工具正在运行在此目录：')
+os.system('echo %cd%')
 
 for q in range(1,114514):# 检查设备列表
-    print('——————————————————————————————————————————')
+    print('—————————————————————————————————————————————————')
     os.system('adb devices')
     os.system('adb shell input swipe 950 1450 950 850 100')
     os.system('adb shell input swipe 950 850 950 1450 100')
-    print('——————————————————————————————————————————')
-    print('检查是否出现设备，一切正常则输入重复次数。输入0重显设备列表。')
-    a=int(input('重复次数：'))
+    print('—————————————————————————————————————————————————')
+    print('检查是否出现设备，一切正常则输入循环次数。输入0重显设备列表，留空回车以退出。')
+    a=int(input('次数：'))
     if a!=0:
         break
+print('如果出现异常，请按下 Ctrl+C 终止刷码进程。出现锁死情况，请在“此循环”进度条在0%~8%时按下“取消”。')
 
 with Progress(TextColumn("[progress.description]{task.description}"),
               BarColumn(),
               TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
               TimeRemainingColumn(),
-              TimeElapsedColumn()) as progress:
-    总进度 = progress.add_task(description="总进度", total=a*4)
+              TimeElapsedColumn()) as progress: # 引用官方Demo
+    总进度 = progress.add_task(description="总进度", total=a*14)
     for ep in range(1):
         每个循环 = progress.add_task(description="此循环", total=14)
         for batch in range(a):
@@ -36,7 +39,6 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                         time.sleep(0.8)
                     if r==3:
                         os.system('adb shell input tap 920 200') # 呼出照片库（在右上角）
-                        progress.advance(总进度, advance=1)
                     if r==4:
                         time.sleep(0.4)
                     if r==5:
@@ -45,7 +47,6 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                         time.sleep(0.3)
                     if r==7:
                         os.system('adb shell input tap 975 140') # 确认照片
-                        progress.advance(总进度, advance=1)
                     if r==8:
                         time.sleep(0.5) # 等待跳转
                     if r==8:
@@ -59,14 +60,12 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                     if r==12:
                         time.sleep(0.5) # 等待跳转
                     if r==13:
-                        progress.advance(总进度, advance=1)
                         os.system('adb shell input swipe 0 1450 150 1450 100') # 返回
                         os.system('adb shell input swipe 0 1450 150 1450 100') # 返回
                     if r==14:
                         os.system('adb shell input tap 685 1370') # 关闭由于跳转失败导致的退出确认窗口
                     r=r+1
                     progress.advance(每个循环, advance=1)
-
-                progress.advance(总进度, advance=1)
+                    progress.advance(总进度, advance=1)
                 
 print('======== 完成 ========')
