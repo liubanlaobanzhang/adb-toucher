d029c3c1e193037a32306403fde9a82fdd0af155
liubanlaobanzhang <3143786339@qq.com>
2023-01-19 18:00:06 +0800

    更改 进度条

--- config/prepare.py
+++ config/prepare.py
@@ -13,17 +13,19 @@ with Progress(TextColumn("[progress.description]{task.description}"),
               TimeRemainingColumn(),
               TimeElapsedColumn()) as progress: # 引用官方Demo
 
-    加载进度 = progress.add_task(description="", total=6)
+    加载进度 = progress.add_task(total=9)
     for load in range(-4,6,1):
             if load==-4: # 获取上次的 QRCODE 随机名
                 with open('config/name',encoding='utf-8') as fileObj1:
                     for line in fileObj1:
                         lastpngname=line.rstrip()
+
             if load==-3:
                  # 获取 QRCODE 的 BASE64 代码
                 with open('config/qrcodebase.txt',encoding='utf-8') as fileObj1:
                     for line in fileObj1:
                         src=line.rstrip()
+
             if load==-2:
                 data = src.split(',')[1]
                 image_data = base64.b64decode(data)
@@ -31,20 +33,25 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                 with open(name, 'wb') as f:
                     f.write(image_data) # 随机命名QRCODE
                     f.close()
+
             if load==-1:
                 # 写入本次的 QRCODE 随机命名
                 f = open('config/name','w') 
                 f.write(name) 
                 f.close()
+
             if load==0:
                 os.system('%cd%/adb/adb.exe shell rm /sdcard/DCIM/Camera/'+lastpngname+' >nul')
+
             if load==1:
                 time.sleep(0.5)
-            if load==3:
+
+            if load==2:
                 os.system('%cd%/adb/adb.exe push '+name+' /sdcard/DCIM/Camera >nul')
-            if load==4:
+
+            if load==3:
                 time.sleep(0.5)
-            if load==5:
+            if load==4:
                 os.system('%cd%/adb/adb.exe shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///sdcard/DCIM/Camera/'+name+' >nul')
                 os.system('del '+name)
             progress.advance(加载进度, advance=1)
