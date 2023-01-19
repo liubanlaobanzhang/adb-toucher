f178baec4819786b8202cca988c3deedd79e30c8
liubanlaobanzhang <3143786339@qq.com>
2023-01-19 17:56:15 +0800

    重构程序架构

---
+++ config/prepare.py
@@ -0,0 +1,50 @@
+import base64
+import random
+from rich.progress import Progress, TextColumn, BarColumn, TimeElapsedColumn, TimeRemainingColumn
+import time
+import os
+
+os.system('adb devices >nul') # 预热adb
+
+print(' Getting things ready…')
+with Progress(TextColumn("[progress.description]{task.description}"),
+              BarColumn(),
+              TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+              TimeRemainingColumn(),
+              TimeElapsedColumn()) as progress: # 引用官方Demo
+
+    加载进度 = progress.add_task(description="", total=6)
+    for load in range(-4,6,1):
+            if load==-4: # 获取上次的 QRCODE 随机名
+                with open('config/name',encoding='utf-8') as fileObj1:
+                    for line in fileObj1:
+                        lastpngname=line.rstrip()
+            if load==-3:
+                 # 获取 QRCODE 的 BASE64 代码
+                with open('config/qrcodebase.txt',encoding='utf-8') as fileObj1:
+                    for line in fileObj1:
+                        src=line.rstrip()
+            if load==-2:
+                data = src.split(',')[1]
+                image_data = base64.b64decode(data)
+                name='qrcode'+str(round(random.randint(1,1145141919810),0))+'.png'
+                with open(name, 'wb') as f:
+                    f.write(image_data) # 随机命名QRCODE
+                    f.close()
+            if load==-1:
+                # 写入本次的 QRCODE 随机命名
+                f = open('config/name','w') 
+                f.write(name) 
+                f.close()
+            if load==0:
+                os.system('%cd%/adb/adb.exe shell rm /sdcard/DCIM/Camera/'+lastpngname+' >nul')
+            if load==1:
+                time.sleep(0.5)
+            if load==3:
+                os.system('%cd%/adb/adb.exe push '+name+' /sdcard/DCIM/Camera >nul')
+            if load==4:
+                time.sleep(0.5)
+            if load==5:
+                os.system('%cd%/adb/adb.exe shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///sdcard/DCIM/Camera/'+name+' >nul')
+                os.system('del '+name)
+            progress.advance(加载进度, advance=1)
--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import time
 import os
 pad="DXQBB22324201093"
 pad2='192.168.3.188:5555'
-nova5pro=""
+nova5pro="6HJ4C19716015113"
 
 print('—————————————————————————————————————————————————')
 print('刷码 2.0.2301(Debug 1)')
 print('—————————————————————————————————————————————————')
         
-os.system('python basepng.py')
+os.system('python %cd%/config/prepare.py')
 
 a=int(input('重复次数：'))
 with Progress(TextColumn("[progress.description]{task.description}"),
@@ -24,26 +24,27 @@ with Progress(TextColumn("[progress.description]{task.description}"),
 
 
         for aqw in range(a):
-            xe=subprocess.run("adb get-serialno",stdout=subprocess.PIPE)
+            xe=subprocess.run("adb/adb get-serialno",stdout=subprocess.PIPE) # 获取设备序列号
             qr=xe.stdout.decode("gbk")
+    
             if qr.strip() == pad or qr.strip() == pad2:
                 progress2.reset(每个循环)
                 progress2.advance(每个循环, advance=1)
                 time.sleep(0.5)
                 for step in range(10):
                     if step==0:
-                        os.system('adb shell input tap 1350 900') # 点击“扫一扫”
+                        os.system('adb/adb shell input tap 1350 900') # 点击“扫一扫”
                     if step==1:
                         time.sleep(0.8)
                     if step==2:
-                        os.system('adb shell input tap 1450 210') # 呼出照片库（在右上角）
+                        os.system('adb/adb shell input tap 1450 210') # 呼出照片库（在右上角）
                         time.sleep(0.3)
                     if step==3:
                         time.sleep(0.2)
-                        os.system('adb shell input tap 150 350') # 选择照片
+                        os.system('adb/adb shell input tap 150 350') # 选择照片
                         time.sleep(0.2)
                     if step==4:
-                        os.system('adb shell input tap 1500 150') # 确认照片
+                        os.system('adb/adb shell input tap 1500 150') # 确认照片
                         time.sleep(0.3) # 等待跳转
                     if step==5:
                         time.sleep(0.5) # 等待跳转
@@ -54,40 +55,38 @@ with Progress(TextColumn("[progress.description]{task.description}"),
                     if step==8:
                         time.sleep(0.5) # 等待跳转
                     if step==9:
-                        os.system('adb shell input swipe 0 1450 350 1450 100') # 返回
-                        os.system('adb shell input swipe 0 1450 350 1450 100') # 返回
+                        os.system('adb/adb shell input swipe 0 1450 350 1450 100') # 返回
+                        os.system('adb/adb shell input swipe 0 1450 350 1450 100') # 返回
                     progress2.advance(每个循环, advance=1)
                     progress2.advance(总进度, advance=1)
 
             if qr.strip() == nova5pro:
-                for step in range(13):
+                progress2.reset(每个循环)
+                progress2.advance(每个循环, advance=1)
+                for step in range(12):
                     if step==0:
-                        os.system('adb shell input tap 850 650') # 点击“扫一扫”
+                        os.system('adb/adb shell input tap 850 650') # 点击“扫一扫”
                     if step==1:
                         time.sleep(0.8)
                     if step==2:
-                        os.system('adb shell input tap 920 200') # 呼出照片库（在右上角）
+                        os.system('adb/adb shell input tap 920 200') # 呼出照片库（在右上角）
                         time.sleep(0.4)
                     if step==3:
-                        os.system('adb shell input tap 150 400') # 选择照片
+                        os.system('adb/adb shell input tap 150 400') # 选择照片
                         time.sleep(0.3)
                     if step==4:
-                        os.system('adb shell input tap 975 140') # 确认照片
+                        os.system('adb/adb shell input tap 975 140') # 确认照片
                         time.sleep(0.5) # 等待跳转
                     if step==5:
-                        time.sleep(0.5) # 等待跳转
+                        time.sleep(0.75) # 等待跳转
                     if step==6:
-                        time.sleep(0.5) # 等待跳转
+                        time.sleep(0.75) # 等待跳转
                     if step==7:
-                        time.sleep(0.5) # 等待跳转
+                        time.sleep(0.75) # 等待跳转
                     if step==8:
-                        time.sleep(0.5) # 等待跳转
+                        os.system('adb/adb shell input swipe 0 1450 150 1450 100') # 返回
+                        os.system('adb/adb shell input swipe 0 1450 150 1450 100') # 返回
                     if step==9:
-                        time.sleep(0.5) # 等待跳转
-                    if step==10:
-                        os.system('adb shell input swipe 0 1450 150 1450 100') # 返回
-                        os.system('adb shell input swipe 0 1450 150 1450 100') # 返回
-                    if step==11:
-                        os.system('adb shell input tap 685 1370') # 关闭由于跳转失败导致的退出确认窗口
+                        os.system('adb/adb shell input tap 685 1370') # 关闭由于跳转失败导致的退出确认窗口
                     progress2.advance(每个循环, advance=1)
                     progress2.advance(总进度, advance=1)
